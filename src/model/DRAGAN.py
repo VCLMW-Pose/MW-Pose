d538194afc16cc32e33749513ac3ee8af4f0df11
K Knight <13558615057@163.com>
2018-11-04 11:25:12 +0800

    Experiment on DRAGAN and MNIST

--- src/model/DRAGAN.py
+++ src/model/DRAGAN.py
@@ -10,12 +10,13 @@ South East University Automation College
 Vision Cognition Laboratory, 211189 Nanjing China
 '''
 
+import os
 import torch
 import torch.nn as nn
 import torch.optim as optim
 import numpy as np
 from torch.autograd import grad
-from src.utils.initialize import initialize_weight
+from src.utils import initialize, printer
 
 class generator(nn.Module):
     def __init__(self, dim_in, dim_out, img_size):
@@ -45,7 +46,7 @@ class generator(nn.Module):
             nn.ConvTranspose2d(64, self.dim_out, 4, 2, 1),
             nn.Tanh()
         )
-        initialize_weight(self)
+        initialize.initialize_weight(self)
 
     def forward(self, x):
         '''
@@ -85,7 +86,7 @@ class discriminator(nn.Module):
             nn.Linear(1024, self.dim_out),
             nn.Sigmoid()
         )
-        initialize_weight(self)
+        initialize.initialize_weight(self)
 
     def forward(self, x):
         x = self.conv(x)
@@ -95,11 +96,88 @@ class discriminator(nn.Module):
         return x
 
 class DRAGAN(nn.Module):
-    def __init__(self, args):
+    def __init__(self, save_dir, result_dir, model_name, gpu_mode, img_size, z_dim, x_dim, lrG, lrD, beta1, beta2):
         '''
         Args:
-
+             save_dir       : (string) directory to save weights
+             result_dir     : (string)
+             gpu_mode       : (boolean)
+             img_size       : (int)
+             z_dim          : (int)
+             x_dim          : (int)
+             lrG            : (float)
+             lrD            : (float)
+             beta1          : (float)
+             beta2          : (float)
         '''
+        self.save_dir = save_dir
+        self.result_dir = result_dir
+        self.model_name = model_name
+        self.img_size = img_size
+
+        self.G = generator(dim_in=z_dim, dim_out=x_dim, img_size=self.img_size)
+        self.D = discriminator(dim_in=x_dim, dim_out=1, img_size=self.img_size)
+        self.G_optimizer = optim.Adam(self.G.parameters(), lr=lrG, betas=(beta1, beta2))
+        self.D_optimizer = optim.Adam(self.D.parameters(), lr=lrD, betas=(beta1, beta2))
+
+        if gpu_mode:
+            self.G.cuda()
+            self.D.cuda()
+
+        print("---------------Network Architecture---------------")
+        printer.print_network(self.G)
+        printer.print_network(self.D)
+        print("--------------------------------------------------")
+
+    def forward(self, z):
+        '''
+        Args:
+             z              : (tensor) input scalar
+        '''
+        return self.G(z)
+
+    def save(self):
+        '''
+        Returns:
+             Save weights of generator and discriminator to corresponding binary file
+        '''
+        # Saving generator weights
+        with open(os.path.join(self.save_dir + self.model_name + '_G'), 'wb') as file:
+            fp = file
+
+        for layer in self.G.modules():
+            # Only deconvolutional layers, linear layers and batch normalization layers need to save weights
+            if isinstance(layer, nn.ConvTranspose2d) or isinstance(layer, nn.Linear):
+                layer.bias.data.cpu().numpy().tofile(fp)
+                layer.weight.data.cpu().numpy().tofile(fp)
+
+            elif isinstance(layer, nn.BatchNorm2d):
+                layer.bias.data.cpu().numpy().tofile(fp)
+                layer.weight.data.cpu().numpy().tofile(fp)
+                layer.running_mean.data.cpu().numpy().tofile(fp)
+                layer.running_var.data.cpu().numpy().tofile(fp)
+
+        fp.close()
+        # Saving discriminator weights
+        with open(os.path.join(self.save_dir + self.model_name + '_D'), 'wb') as file:
+            fp = file
+
+        for layer in self.D.modules():
+            # Only convolutional layers, linear layers and batch normalization layers need to save weights
+            if isinstance(layer, nn.Conv2d) or isinstance(layer, nn.Linear):
+                layer.bias.data.cpu().numpy().tofile(fp)
+                layer.weight.data.cpu().numpy().tofile(fp)
+
+            elif isinstance(layer, nn.BatchNorm2d):
+                layer.bias.data.cpu().numpy().tofile(fp)
+                layer.weight.data.cpu().numpy().tofile(fp)
+                layer.running_mean.data.cpu().numpy().tofile(fp)
+                layer.running_var.data.cpu().numpy().tofile(fp)
+
+        fp.close()
+
+    #def load(self):
+
 
 
 
---
+++ src/utils/printer.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+'''
+    Created on Sun Nov 4 10:46 2018
+
+    Author           : Shaoshu Yang
+    Email            : shaoshuyangseu@gmail.com
+    Last edit date   : Sat Sun Nov 4 24:00 2018
+
+South East University Automation College
+Vision Cognition Laboratory, 211189 Nanjing China
+'''
+
+import torch
+import torch.nn as nn
+import numpy as np
+
+def print_network(network):
+    '''
+    Args:
+         network        : (nn.Module) input network
+    '''
+    num_params = 0
+    for param in network.parameters():
+        num_params += param.numel()
+
+    print(network)
+    print("Total parameters: %d" % num_params)
