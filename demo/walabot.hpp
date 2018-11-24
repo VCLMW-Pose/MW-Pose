4df6ca5ad177668c283033eedf72b5bdeb51efbb
K Knight <13558615057@163.com>
2018-11-24 23:43:33 +0800

    Commit C++ walabot API

--- demo/walabot.hpp
+++ demo/walabot.hpp
@@ -8,16 +8,27 @@
 
 #ifndef DEMO_WALABOT_HPP
 #define DEMO_WALABOT_HPP
-#include <WalabotAPI.h>
 
-typedef double _angular_arena;
-typedef double _depth_arena;
-typedef double _depth_resol;
-typedef double _angular_resol;
+#include <WalabotAPI.h>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <exception>
+#include <stdexcept>
+
+using namespace cv;
+
+typedef double _angular_arena;                                                                      // Angular arena scale in angle
+typedef double _depth_arena;                                                                        // Depth arena scale in centimeter
+typedef double _depth_resol;                                                                        // Depth resolution in centimeter
+typedef double _angular_resol;                                                                      // Angular resolution in angle
 typedef bool _filter;
+typedef int _scan_prof;                                                                             // Scan profile
 
 class walabot
 {
+    const _scan_prof SCAN_HORIZONTAL = 0;                                                           // Project energies to horizontal plane
+    const _scan_prof SCAN_PERPENDICULAR = 1;                                                        // Project energies to perpendicular plane
+
 private:
     /* Basic coefficient of walabot scanning profile.*/
     _depth_arena _r_min;                                                                            // Minimum depth of scanning arena
@@ -35,20 +46,39 @@ private:
     _filter _MTI;                                                                                   // Applying dynamic reflector filter
 
 public:
+    /* Default constructor and destructor of walabot class. Walabot must be intialized with a
+     * set of coefficients. The constructor routine would not initialize walabot, hence it can
+     * not perform scanning immediately. The defualt destructor would disconncect walabot automatically.*/
     walabot(_depth_arena r_min, _depth_arena r_max, _depth_resol r_res, _angular_arena phi_min, _angular_arena phi_max,
                     _angular_resol phi_res, _angular_arena theta_min, _angular_arena theta_max, _angular_resol theta_res);
-    ~walabot();
-    
-    bool start();
-    bool disconnect();
-    
-    bool set_phi();
-    bool set_theta();
-    bool set_r();
+                                                                                                    // Defualt constructor
+    ~walabot();                                                                                     // Defualt destructor
 
-    bool
+    /* State control routine for walabot. After applying start routine, walabot can perform scanning and
+     * collecting images. disconnect routine provides manual shut down means to modify the scanning
+     * profiles.*/
+    bool start();                                                                                   // Start up
+    bool disconnect();                                                                              // Shut down
 
+    /* Configuration routines for walabot. Ensure disconnect is applied before adopting these coefficients
+     * modification routines. They were for horizontal, perpendicular and depth parameters modification
+     * separately. */
+    bool set_phi(const _angular_arena phi_min, const _angular_arena phi_max, const _angular_resol phi_res);
+    bool set_theta(const _angular_arena theta_min, const _angular_arena theta_max, const _angular_resol theta_res);
+    bool set_r(const _depth_arena r_min, const _depth_arena r_max, const _depth_resol r_res);
+                                                                                                    // Phi scale modification
+                                                                                                    // Theta scale modification
+                                                                                                    // Depth scale modification
+
+    bool set_filter(const _filter filter);                                                          // Reset motion target identification filter
+
+    /* Scanning routine for walabot. The direction of projection can be determined by the parameter scan_prof.
+     * These routines stores collected data in cv::Mat. */
+    Mat & get_frame(const _scan_prof scan_prof);                                                    // Get single frame
+    Mat * scan(const _scan_prof scan_prof);                                                         // Get projections over both planes
+
+    /* Private test routines. Provide visual outputs of scanning profiles*/
+    void _scan_test(const _scan_prof scan_prof);                                                    // Scan test
 };
-
 
 #endif //DEMO_WALABOT_HPP
